b882e72dec84d0b7c03d81dc48709dcf9e81e4fe
OthmaELbei <osamazayd1321@gmail.com>
2023-11-16 16:21:07 +0100

    add timing

--- vite-project/src/component/MeinContent.jsx
+++ vite-project/src/component/MeinContent.jsx
@@ -11,13 +11,19 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import axios from "axios";
+import moment from "moment";
 import { useState, useEffect } from "react";
+import "moment/dist/locale/ar-dz";
+moment.locale("ar");
 
 export default function MeinContent() {
   const [slactCity, setSlactCity] = useState({
     displayName: "سطات",
     apiName: "Settat",
   });
+  const [remainingTime, setRemainingTime] = useState("");
+  const [today, setToday] = useState("");
+  const [intervel, setIntervel] = useState("10");
   const avilabelCities = [
     {
       displayName: "سطات",
@@ -52,6 +58,14 @@ export default function MeinContent() {
       apiName: "Meknes",
     },
   ];
+  const [nextPrayerIndex, setNextPrayerIndex] = useState(3);
+  const prayersArray = [
+    { key: "Fajr", displayName: "الفجر" },
+    { key: "Dhuhr", displayName: "الضهر" },
+    { key: "Asr", displayName: "العصر" },
+    { key: "Maghrib", displayName: "المغرب" },
+    { key: "Isha", displayName: "العشاء" },
+  ];
 
   const [timings, setTimings] = useState({
     Fajr: "",
@@ -73,6 +87,79 @@ export default function MeinContent() {
     getTimings();
   }, [slactCity]);
 
+  useEffect(() => {
+    let interval = setIntervel(() => {
+      console.log("clling timer");
+      setupCountdownTimer();
+    }, 1000);
+
+    const t = moment();
+    setToday(t.format("MMM Do YYYY |h:mm"));
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const setupCountdownTimer = () => {
+    const momentNow = moment();
+    let prayerIndex = 2;
+
+    if (
+      momentNow.isAfter(
+        moment(timings["Fajr"], "hh:mm") &&
+          momentNow.isBefore(moment(timings["Dhuhr"], "hh:mm"))
+      )
+    ) {
+      prayerIndex = 1;
+    } else if (
+      momentNow.isAfter(
+        moment(timings["Dhuhr"], "hh:mm") &&
+          momentNow.isBefore(moment(timings["Asr"], "hh:mm"))
+      )
+    ) {
+      prayerIndex = 2;
+    } else if (
+      momentNow.isAfter(
+        moment(timings["Asr"], "hh:mm") &&
+          momentNow.isBefore(moment(timings["Maghrib"], "hh:mm"))
+      )
+    ) {
+      prayerIndex = 3;
+    } else if (
+      momentNow.isAfter(
+        moment(timings["Maghrib"], "hh:mm") &&
+          momentNow.isBefore(moment(timings["Isha"], "hh:mm"))
+      )
+    ) {
+      prayerIndex = 4;
+    } else {
+      prayerIndex = 0;
+    }
+    setNextPrayerIndex(prayerIndex);
+
+    const nextPrayerOpject = prayersArray[prayerIndex];
+    const nextPrayerTimer = timings[nextPrayerOpject.key];
+    const nextprayerTimeMoment = moment(nextPrayerTimer, "hh:mm");
+    let remainingTime = moment(nextPrayerTimer, "hh:mm").diff(momentNow);
+
+    if (remainingTime < 0) {
+      const midnightDiff = moment("23:59:59", "hh:mm:ss").diff(momentNow);
+      const fajrToMidnightDiff = nextprayerTimeMoment.diff(
+        moment("00:00:00", "hh:mm:ss")
+      );
+      console.log(fajrToMidnightDiff);
+      const totalDiffernce = midnightDiff + fajrToMidnightDiff;
+
+      remainingTime = totalDiffernce;
+    }
+    console.log(remainingTime);
+    const durationRemainingTime = moment.duration(remainingTime);
+
+    setRemainingTime(
+      `${durationRemainingTime.hours()}:${durationRemainingTime.minutes()}:${durationRemainingTime.seconds()}`
+    );
+  };
+
   const handleCityChange = (event) => {
     const cityObject = avilabelCities.find((city) => {
       return city.apiName == event.target.value;
@@ -85,12 +172,13 @@ export default function MeinContent() {
       {/* top role */}
       <Grid container>
         <Grid xs={6}>
-          <h3>4:20 | 2023 9 سبتمبر</h3>
+          <h3>{today}</h3>
           <h4>{slactCity.displayName}</h4>
+          <h3>{intervel}</h3>
         </Grid>
         <Grid xs={6}>
-          <h3>المتبقي حتى صلاة العصر</h3>
-          <h2>00:10:20</h2>
+          <h3> المتبقي حت صلاة {prayersArray[nextPrayerIndex].displayName}</h3>
+          <h2>{remainingTime}</h2>
         </Grid>
       </Grid>
       {/* == top role == */}
@@ -145,7 +233,9 @@ export default function MeinContent() {
           >
             {avilabelCities.map((city) => {
               return (
-                <MenuItem value={city.apiName}>{city.displayName}</MenuItem>
+                <MenuItem key={city.apiName} value={city.apiName}>
+                  {city.displayName}
+                </MenuItem>
               );
             })}
           </Select>
